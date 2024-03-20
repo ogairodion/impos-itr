c115266c4189ad53ba262dd3161fff17ca321c21
ogairodion <ogairodion@gmail.com>
2024-03-20 16:44:21 +0300

    подготовка к верстке

---
+++ gulpfile.babel.js
@@ -0,0 +1,78 @@
+"use strict";
+
+import gulp from "gulp";
+
+const requireDir = require("require-dir"),
+    paths = {
+        views: {
+            src: [
+                "./src/views/index.pug",
+                "./src/views/pages/*.pug"
+            ],
+            dist: "./dist/",
+            watch: [
+                "./src/blocks/**/*.pug",
+                "./src/views/**/*.pug"
+            ]
+        },
+        styles: {
+            src: ["./src/styles/*.{scss,sass}"],
+            dist: "./dist/styles/",
+            watch: [
+                "./src/blocks/**/*.{scss,sass}",
+                "./src/styles/**/*.{scss,sass}"
+            ]
+        },
+        scripts: {
+            src: "./src/js/index.js",
+            dist: "./dist/js/",
+            watch: [
+                "./src/blocks/**/*.js",
+                "./src/js/**/*.js"
+            ]
+        },
+        images: {
+            src: [
+                "./src/img/**/*.{jpg,jpeg,png,gif,tiff,svg}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ],
+            dist: "./dist/img/",
+            watch: "./src/img/**/*.{jpg,jpeg,png,gif,svg}"
+        },
+        videos: {
+          src: "./src/videos/*.mp4",
+          dist: "./dist/videos/",
+          watch: "./src/videos/*.mp4"
+        },
+        sprites: {
+            src: "./src/img/svg/*.svg",
+            dist: "./dist/img/sprites/",
+            watch: "./src/img/svg/*.svg"
+        },
+        fonts: {
+            src: "./src/fonts/**/*.{woff,woff2}",
+            dist: "./dist/fonts/",
+            watch: "./src/fonts/**/*.{woff,woff2}"
+        },
+        favicons: {
+            src: "./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}",
+            dist: "./dist/img/favicons/",
+        },
+        gzip: {
+            src: "./src/.htaccess",
+            dist: "./dist/"
+        }
+    };
+
+requireDir("./gulp-tasks/");
+
+export { paths };
+
+export const development = gulp.series("clean",
+    gulp.parallel(["views", "styles", "scripts", "images", "webp", "videos", "sprites", "fonts", "favicons"]),
+    gulp.parallel("serve"));
+
+export const prod = gulp.series("clean",
+    gulp.parallel(["views", "styles", "scripts", "images", "webp", "videos", "sprites", "fonts", "favicons", "gzip"]));
+
+export default development;
