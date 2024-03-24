848d2d8bb483042d125b76c493082a7411b35895
ogairodion <ogairodion@gmail.com>
2024-03-24 20:13:26 +0300

    sliders

--- src/blocks/modules/scheme/scheme.js
+++ src/blocks/modules/scheme/scheme.js
@@ -3,38 +3,60 @@ import { Navigation, EffectFade, Pagination, Thumbs } from 'swiper/modules';
 
 const tabsContent = document.querySelectorAll('.tabs__content');
 
-tabsContent.forEach((content, id) => {
-  const schemeSlider = content.querySelector('.scheme__slider');
-  const schemeSliderInfo = content.querySelector('.scheme__slider-info');
+function callback(mutationsList) {
+  mutationsList.forEach((element) => {
+    if (!element.target.classList.contains('hidden')) {
+      getSliders(element.target);
+    }
+  });
+}
 
-  schemeSliderInfo.classList.add(`scheme__slider-info-${id}`);
+const mutationObserver = new MutationObserver(callback);
 
-  new Swiper(schemeSlider, {
+tabsContent.forEach((content) => {
+  mutationObserver.observe(content, { attributes: true });
+});
+
+function getSliders(element) {
+  let slider = element.querySelector('.scheme__slider');
+  let sliderInfo = element.querySelector('.scheme__slider-info');
+  let arrowLeft = element.querySelector('.slider-navigation-arrow--prev');
+  let arrowRight = element.querySelector('.slider-navigation-arrow--next');
+
+  const sliders = sliderInfo.querySelectorAll('.swiper-slide');
+  const sliderLength = element.querySelector('.slider-navigation-progress__length');
+  const sliderActiveIndex = element.querySelector('.slider-navigation-progress__active');
+
+  sliderLength.innerText = sliders.length;
+  sliderActiveIndex.innerText = 1;
+
+  slider = new Swiper(slider, {
     modules: [Navigation, EffectFade],
     slidesPerView: 1,
     effect: 'fade',
     fadeEffect: {
       crossFade: true
     },
-    speed: 1000,
-    loop: true,
   });
 
-  new Swiper(schemeSliderInfo, {
+  sliderInfo = new Swiper(sliderInfo, {
     modules: [Navigation, EffectFade, Thumbs],
     slidesPerView: 1,
     effect: 'fade',
     fadeEffect: {
       crossFade: true
     },
-    speed: 1000,
-    loop: true,
     navigation: {
-      nextEl: `.scheme__slider-info-${id} .slider-navigation-arrow--next`,
-      prevEl: `.scheme__slider-info-${id} .slider-navigation-arrow--prev`,
+      nextEl: arrowRight,
+      prevEl: arrowLeft,
+    },
+    on: {
+      slideChange() {
+        sliderActiveIndex.innerText = this.activeIndex + 1;
+      },
     },
     thumbs: {
-      swiper: schemeSlider,
-    }
+      swiper: slider,
+    },
   });
-});
+}
