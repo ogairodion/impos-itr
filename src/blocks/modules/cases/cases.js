98053c40bce4cb0379902068ee5467acb002b72b
ogairodion <ogairodion@gmail.com>
2024-04-01 00:57:02 +0300

    cases

---
+++ src/blocks/modules/cases/cases.js
@@ -0,0 +1,48 @@
+import Swiper from 'swiper';
+import { Navigation, EffectFade } from 'swiper/modules';
+
+const cases = document.querySelector('.cases');
+const items = cases.querySelectorAll('.cases__item');
+let headerHeight = document.querySelector('.header').offsetHeight;
+let diff = headerHeight;
+
+if (items.length) {
+  items.forEach((item, id) => {
+    let slider = item.querySelector('.swiper');
+    const slides = item.querySelectorAll('.swiper-slide');
+    const arrowNext = item.querySelector('.slider-navigation-arrow--next');
+    const arrowPrev = item.querySelector('.slider-navigation-arrow--prev');
+    const slidesLength = item.querySelector('.slider-navigation-progress__length');
+    const slidesActive = item.querySelector('.slider-navigation-progress__active');
+
+    slidesLength.innerText = slides.length;
+
+    arrowNext.classList.add(`swiper-arrow-next-${id}`);
+    arrowPrev.classList.add(`swiper-arrow-prev-${id}`);
+    slider.classList.add(`swiper-slider-${id}`);
+
+    new Swiper(`.swiper-slider-${id}`, {
+      modules: [Navigation, EffectFade],
+      slidesPerView: 1,
+      spaceBetween: 8,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      speed: 1000,
+      navigation: {
+        nextEl: `.swiper-arrow-next-${id}`,
+        prevEl: `.swiper-arrow-prev-${id}`,
+      },
+      loop: true,
+      on: {
+        slideChange: function () {
+          slidesActive.innerText = this.realIndex + 1;
+        },
+      },
+    });
+
+    item.style.top += `${diff}px`;
+    diff += 64;
+  });
+}
--- src/js/main-page.js
+++ src/js/main-page.js
@@ -4,3 +4,4 @@ import "%modules%/team/team";
 import "%modules%/reviews/reviews-messengers";
 import "%modules%/reviews/reviews-letters";
 import "%modules%/certificates/certificates";
+import "%modules%/cases/cases";
