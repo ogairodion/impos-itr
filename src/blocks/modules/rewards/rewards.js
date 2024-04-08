36af0e64a7fc5bf4cbc73d3e186163f36b3f3918
ogairodion <ogairodion@gmail.com>
2024-04-09 01:58:25 +0300

    adaptive

--- src/blocks/modules/rewards/rewards.js
+++ src/blocks/modules/rewards/rewards.js
@@ -6,19 +6,30 @@ const slides = parent.querySelectorAll('.swiper-slide');
 const slidesLength = parent.querySelector('.slider-navigation-progress__length');
 const slidesActive = parent.querySelector('.slider-navigation-progress__active');
 
+let windowWidth = 0;
+windowWidth = window.innerWidth;
 slidesLength.innerText = slides.length;
 
 const rewardsSlider = new Swiper('.rewards__slider', {
   modules: [Navigation],
   slidesPerView: 'auto',
-  spaceBetween: 18,
+  spaceBetween: 8,
   navigation: {
     nextEl: '.rewards .slider-navigation-arrow--next',
     prevEl: '.rewards .slider-navigation-arrow--prev',
   },
+  centeredSlides: true,
+  loop: true,
   on: {
     slideChange: function () {
       slidesActive.innerText = this.realIndex + 1;
+    },
+  },
+  breakpoints: {
+    1200: {
+      centeredSlides: false,
+      spaceBetween: 18,
+      loop: false,
     },
   },
 });
