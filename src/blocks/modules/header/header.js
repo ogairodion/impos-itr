d7e097bf5fcfee7d0c81c83d247b47ecb8552f9c
ogairodion <ogairodion@gmail.com>
2024-04-22 04:24:25 +0300

    header hide

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -5,3 +5,16 @@ const phones = document.querySelectorAll('input[type="phone"]');
 const im = new Inputmask('+7(999)-999-99-99');
 
 im.mask(phones);
+
+let scrollBefore = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (scrollBefore <= window.scrollY && window.scrollY > header.offsetHeight) {
+    header.classList.add('header--hide');
+  } else {
+    header.classList.remove('header--hide');
+  }
+
+  scrollBefore = window.scrollY;
+});
