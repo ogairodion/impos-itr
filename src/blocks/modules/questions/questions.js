b42874b0c0f9a1b4819e1fcbe9bd5ae3cd442cf2
ogairodion <ogairodion@gmail.com>
2024-04-24 22:06:02 +0300

    fixes

---
+++ src/blocks/modules/questions/questions.js
@@ -0,0 +1,23 @@
+const questions = document.querySelector('.questions');
+const questionsList = questions.querySelectorAll('.dropdown');
+const questionsMore = questions.querySelector('.button--more');
+
+if (questionsList.length) {
+  questionsList.forEach((question, index) => {
+    const number = question.querySelector('.dropdown__number');
+
+    number.innerText = index + 1 < 10 ? `0${index + 1}` : index + 1;
+
+    if (index > 4) {
+      question.classList.add('hidden');
+    }
+  });
+
+  questionsMore.addEventListener('click', () => {
+    questionsList.forEach((question) => {
+      question.classList.remove('hidden');
+    });
+
+    questionsMore.classList.add('hidden');
+  });
+}
--- src/js/service.js
+++ src/js/service.js
@@ -1,5 +1,6 @@
 import "%components%/map/map";
 import "%modules%/main-top/main-top";
+import "%modules%/questions/questions";
 import "%modules%/scheme/scheme";
 import "%modules%/rates/rates";
 import "%modules%/services/services";
